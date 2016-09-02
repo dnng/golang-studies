4789dd586614ba8cb6542e460354292f7e055ed3
Daniel Noguchi <danielnoguchi@gmail.com>
2016-09-02 03:10:25 -0300

    Fix indent

--- nogoutdupname.go
+++ nogoutdupname.go
@@ -3,40 +3,39 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
-    counts := make(map[string]int)
-    files := os.Args[1:]
-    if len(files) == 0 {
-        countLines(os.Stdin, counts)
-    } else {
-        for _, arg := range files {
-            f, err := os.Open(arg)
-            if err != nil {
-                fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-                continue
-            }
-            countLines(f, counts)
-            f.Close()
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
 			for line, n := range counts {
 				if n > 1 {
 					fmt.Printf("%s:\t%d\t%s\n", arg, n, line)
 				}
 			}
 			counts = make(map[string]int)
-        }
+		}
 	}
 }
 
 func countLines(f *os.File, counts map[string]int) {
-    input := bufio.NewScanner(f)
-    for input.Scan() {
-        counts[input.Text()]++
-    }
-    // NOTE: Ignoring potential errors from input.Err()
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: Ignoring potential errors from input.Err()
 }
-
