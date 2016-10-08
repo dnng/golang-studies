0782171dd87998958ac047a7732b6c93008fdcd6
Daniel Noguchi <danielnoguchi@gmail.com>
2016-10-08 19:43:47 -0300

    Add solution to exercise 3.3

--- surface.go
+++ surface.go
@@ -24,39 +24,45 @@ func main() {
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay := corner(i+1, j)
-			bx, by := corner(i, j)
-			cx, cy := corner(i, j+1)
-			dx, dy := corner(i+1, j+1)
+			ax, ay, _ := corner(i+1, j)
+			bx, by, _ := corner(i, j)
+			cx, cy, _ := corner(i, j+1)
+			dx, dy, color := corner(i+1, j+1)
 
 			// Solution for exercise 3.1
 			if math.IsInf(ax, 0) || math.IsInf(bx, 0) || math.IsInf(cx, 0) || math.IsInf(dx, 0) {
 				continue
 			}
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy)
+			fmt.Printf("<polygon style='fill: %s' points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				color, ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
 	fmt.Println("</svg>")
 }
 
-func corner(i, j int) (float64, float64) {
+func corner(i, j int) (float64, float64, string) {
 	// Find point (x,y) at corner of cell (i, j).
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
+	var co string
 
 	// Compute surface height z.
 	z := f(x, y)
+	if z >= 0 {
+		co = "#ff0000"
+	} else {
+		co = "#0000ff"
+	}
 
 	// Solution for exercise 3.1
 	if math.IsInf(z, 0) {
-		return math.Inf(0), math.Inf(0)
+		return math.Inf(0), math.Inf(0), co
 	}
 
 	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
-	return sx, sy
+	return sx, sy, co
 }
 
 func f(x, y float64) float64 {
