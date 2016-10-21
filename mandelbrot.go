361ba50d6edd6ad9aedbf7ae6d4e809d9f811224
Daniel Noguchi <danielnoguchi@gmail.com>
2016-10-21 15:11:13 -0200

    Solution for 3.5

--- mandelbrot.go
+++ mandelbrot.go
@@ -36,7 +36,8 @@ func mandelbrot(z complex128) color.Color {
 	for n := uint8(0); n < interactions; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
-			return color.Gray{255 - contrast*n}
+			// return color.Gray{255 - contrast*n}
+			return color.RGBA{15, 125, 204,  contrast*n}
 		}
 	}
 	return color.Black
